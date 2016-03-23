348dac72f4ecd5941f1f65690f8c9667bb87753b
Joakim Berntsson <jcberntsson@gmail.com>
2016-03-23 09:43:08 +0100

    Fixed mongo install prov

--- mongo/prov.sh
+++ mongo/prov.sh
@@ -1,12 +1,12 @@
 
-# Install MySQL Server
+# Install MongoDB Server
 echo "Provisioning"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 
-echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
-
+echo "Install mongo"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo apt-get install mongodb-org
 
 echo "Fixing mongo binding"
 sed -i "/bindIp/d" /etc/mongod.conf
