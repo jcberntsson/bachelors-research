8c2af02c666481fc106bfbac4b4023362aa735a4
Scanniza <simon@cymonit.se>
2016-04-26 13:54:05 +0200

    dsf

--- tests/mysql_test.py
+++ tests/mysql_test.py
@@ -56,9 +56,6 @@ def reddit_createTables(cnx):
         "  CONSTRAINT post_fk FOREIGN KEY (post_id) "
         "     REFERENCES post (id) ON DELETE CASCADE"
         ") ENGINE=InnoDB")
-    cursor.execute('drop table comment');
-    cursor.execute('drop table upvote');
-    cursor.execute('drop table downvote');
     for name, ddl in TABLES.items():
         try:
             print("Creating table {}: ".format(name), end='')
