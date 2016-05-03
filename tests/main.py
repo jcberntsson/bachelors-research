fcf3ebf22e2821cda22191b8f73670d3abdd5e64
Scanniza <simon@cymonit.se>
2016-05-03 19:50:12 +0200

    fetch participants

--- tests/main.py
+++ tests/main.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python
+from cases import MySQL
 
-<<<<<<< HEAD
-from cases import Mongo
-
-# Run
+'''# Run
 if __name__ == '__main__':
 
     m = Mongo()
 
     m.init("skim")
 =======
-from cases import Neo4j
+from cases import Neo4j'''
 
 # Run
 if __name__ == '__main__':
-    neo = Neo4j()
-    #neo.init("raceone")
-    case = neo.fetchUsers()
+    m = MySQL()
+    m.init("raceone")
+    case = m.fetchParticipants()
     case.run()
->>>>>>> 7e28094ee25e5ec49aa7b279298031bed756ac24
-    #testCase = neo.commentOnImage()
