22f7f4c9cd5f14710e330e7092426acb5bb551e2
Scanniza <simon@cymonit.se>
2016-05-04 09:08:44 +0200

    first sql benchmarking

--- tests/benchmarking.py
+++ tests/benchmarking.py
@@ -27,7 +27,7 @@ print("Extracting arguments")
 # Extract arguments
 database = argv[1]
 company = argv[2]
-iterations = 1000   # Number of runs per test case
+iterations = 10000   # Number of runs per test case
 
 # Test arguments
 validCompany = (company == "raceone" or company == "skim" or company == "reddit")
