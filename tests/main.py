68b9688b11a64ab991dde12d9f6c5bc825e0792f
Pedram Talebi <pedram.talebi18@gmail.com>
2016-05-07 16:05:12 +0200

    mongodb work

--- tests/main.py
+++ tests/main.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
-
-<<<<<<< HEAD
 from cases import Mongo
 
 # Run
@@ -9,14 +7,4 @@ if __name__ == '__main__':
     m = Mongo()
 
     m.init("skim")
-=======
-from cases import Neo4j
-
-# Run
-if __name__ == '__main__':
-    neo = Neo4j()
-    #neo.init("raceone")
-    case = neo.fetchUsers()
-    case.run()
->>>>>>> 7e28094ee25e5ec49aa7b279298031bed756ac24
     #testCase = neo.commentOnImage()
