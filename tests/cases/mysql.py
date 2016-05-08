8ffcee9fd00d14602c49aed1a79145d0dd0ca859
Scanniza <simon@cymonit.se>
2016-05-08 14:34:53 +0200

    fetch hot races

--- tests/cases/mysql.py
+++ tests/cases/mysql.py
@@ -839,7 +839,7 @@ class MySQL(Base):
             cursor = self.cnx.cursor()
             '''cursor.execute("SELECT activity.race, count(*) as rating FROM activity "
                 "INNER JOIN follow on follow.activity=activity.id GROUP BY activity.race ORDER BY rating LIMIT 10")'''
-            cursor.execute("SELECt activity.race,count(*) FROM activity INNER JOIN follow on follow.activity=activity.id GROUP BY activity.race")
+            cursor.execute("SELECt activity.race,count(*) as rating FROM activity INNER JOIN follow on follow.activity=activity.id GROUP BY activity.race ORDER BY rating DESC")
             result = cursor.fetchall()
             print(result)
             cursor.close()
