4b9e4559c84bda34b47aab4244a5b0cdeea2fbe5
Pedram Talebi <pedram.talebi18@gmail.com>
2016-05-10 09:37:10 +0200

    mongodb testing

--- tests/main.py
+++ tests/main.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
+    
 from cases import Mongo
-
 # Run
 if __name__ == '__main__':
 
     m = Mongo()
-
-    m.init("skim")
+    m.init("raceone")
     #testCase = neo.commentOnImage()
 
