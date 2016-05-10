59c931088cef0f83f7a8269d12c8289ceae470df
Scanniza <simon@cymonit.se>
2016-05-10 16:44:32 +0200

    fetch race

--- tests/cases/mysql.py
+++ tests/cases/mysql.py
@@ -986,8 +986,8 @@ class MySQL(Base):
                 "INNER JOIN point as p2 ON racemap.goal_point = p2.id WHERE race.ID="+inner_self.race_id)
             cursor.execute("SELECT * FROM race INNER JOIN event ON race.event_id=event.id "+
                 "INNER JOIN racemap ON racemap.race = race.id "+
-                "INNER JOIN point as p1 ON racemap.start_point = p1.id "+
-                "INNER JOIN point as p2 ON racemap.goal_point = p2.id WHERE race.ID="+inner_self.race_id)
+                "LEFT JOIN point as p1 ON racemap.start_point = p1.id "+
+                "LEFT JOIN point as p2 ON racemap.goal_point = p2.id WHERE race.ID="+inner_self.race_id)
             result = cursor.fetchall()
             print(result)
             cursor.close()
