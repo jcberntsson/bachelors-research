192f87c0570be37c29f552dd7c7b59791af15b96
Scanniza <simon@cymonit.se>
2016-05-10 19:39:01 +0200

    pul

--- tests/main.py
+++ tests/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-from cases import Neo4j
+from cases import MySQL
 
 # Run
 if __name__ == '__main__':
-    neo = Neo4j()
+    m = MySQL()
     #neo.init("raceone")
-    case = neo.fetchCoords()
+    case = m.follow()
     case.setup()
     case.run()
     case.teardown()
