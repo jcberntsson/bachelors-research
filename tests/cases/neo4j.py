62b5630085a9cdd35ab41ac3a8b5b6bb634cb12e
Joakim Berntsson <jcberntsson@gmail.com>
2016-05-11 11:38:38 +0200

    aa

--- tests/cases/neo4j.py
+++ tests/cases/neo4j.py
@@ -8,7 +8,7 @@ from neo4j.v1 import GraphDatabase, basic_auth
 class Neo4j(Base):
     driver = GraphDatabase.driver("bolt://46.101.235.47", auth=basic_auth("neo4j", "kandidat"))
     #driver = GraphDatabase.driver("bolt://10.135.10.154", auth=basic_auth("neo4j", "kandidat"))
-    session = driver.session()
+
 
     # connect to authenticated graph database
     # graph = Graph("http://neo4j:kandidat@localhost:7474/db/data/")
@@ -780,16 +780,10 @@ class Neo4j(Base):
             pass
 
         def run(inner_self):
-            #out = self.graph.run(
-            #    'MATCH (n) '
-            #    'RETURN n'
-            #)
-            result = self.session.run("MATCH (n) RETURN n")
+            session = self.driver.session()
+            result = session.run("MATCH (n) RETURN n")
             retained_result = list(result)
-            #for record in result:
-            #    print(record)
-            #while out.forward():
-            #    print(out.current)
+            session.close()
 
         def teardown(inner_self):
             pass
