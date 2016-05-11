761b59c4489440284bf70d307c6d60b8954f77f3
Joakim Berntsson <jcberntsson@gmail.com>
2016-05-11 23:09:48 +0200

    fix

--- tests/cases/mongo.py
+++ tests/cases/mongo.py
@@ -7,8 +7,8 @@ import datetime
 
 class Mongo(Base):
     # connect to authenticated mongo database
-    client = MongoClient("mongodb://46.101.103.26:27017")
-    #client = MongoClient("mongodb://10.135.3.156:27017")
+    #client = MongoClient("mongodb://46.101.103.26:27017")
+    client = MongoClient("mongodb://10.135.3.156:27017")
     db = client.db
 
     ####################################
