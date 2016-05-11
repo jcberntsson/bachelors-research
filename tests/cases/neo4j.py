8b1f2ad19fe07c3a8ee6a5a00b15fd1743bc871a
Joakim Berntsson <jcberntsson@gmail.com>
2016-05-11 10:44:06 +0200

    aa

--- tests/cases/neo4j.py
+++ tests/cases/neo4j.py
@@ -774,15 +774,15 @@ class Neo4j(Base):
             pass
 
         def run(inner_self):
-            out = self.graph.run(
-                'MATCH (n) '
-                'RETURN n'
-            )
+            #out = self.graph.run(
+            #    'MATCH (n) '
+            #    'RETURN n'
+            #)
             #while out.forward():
             #    print(out.current)
-            #out = self.graph.find("TEST")
-            #for test in out:
-            #    a = test
+            out = self.graph.find("TEST")
+            for test in out:
+                a = test
 
         def teardown(inner_self):
             pass
