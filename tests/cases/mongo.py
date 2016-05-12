71199766928c2211cc9698b8f556c4658f430fec
Joakim Berntsson <jcberntsson@gmail.com>
2016-05-12 22:45:05 +0200

    aa

--- tests/cases/mongo.py
+++ tests/cases/mongo.py
@@ -4,8 +4,8 @@ import random
 from random import randint
 import datetime
 
-class Mongo(Base):
 
+class Mongo(Base):
     # connect to authenticated mongo database
     # client = MongoClient("mongodb://46.101.103.26:27017")
     client = MongoClient("mongodb://10.135.3.156:27017")
@@ -170,7 +170,7 @@ class Mongo(Base):
                         "bitDepth": 15,
                         "createdAt": "2016-03-03",
                         "accepted": False,
-                        "comments": comment
+                        "comments": comments
                     }).inserted_id)
 
                 sku_list.append(self.db.skus.insert_one({
@@ -197,7 +197,7 @@ class Mongo(Base):
     ############################
     ####	TEST METHODS	####
     ############################
-    
+
     # REFERENCE
     def tinyGet(self):
         def setup(inner_self):
@@ -271,59 +271,66 @@ class Mongo(Base):
                 {"_id": inner_self.image_id},
                 {"$pull": {"comments": {"text": "Ooh, another new comment!"}}}
             )
+
         return self.create_case("commentOnImage", setup, run, teardown)
-    
+
     def pairImageSKU(self):
         def setup(inner_self):
             inner_self.project_id = self.get_random_id("projects")
             out = self.db.projects.find_one({"_id": inner_self.project_id})
-            
+
             inner_self.sku_id = out["skus"][0]
             inner_self.image_id = out["images"][0]
-            
+
         def run(inner_self):
             self.db.skus.update(
                 {"_id": inner_self.sku_id},
                 {"$push": {"images_sku": inner_self.image_id}}
             )
-            
+
         def teardown(inner_self):
             self.db.skus.update(
                 {"_id": inner_self.sku_id},
                 {"$pull": {"images_sku": inner_self.image_id}}
             )
+
         return self.create_case("pairImageSKU", setup, run, teardown)
-    
+
     def addRowsToSKU(self):
         def setup(inner_self):
             inner_self.project_id = self.get_random_id("projects")
             sku = {
                 "name": "sku_91239",
-                "sku_values": {"header":"header_new", "value": "new"}
+                "sku_values": {"header": "header_new", "value": "new"}
             }
             inner_self.sku = sku
+
         def run(inner_self):
             inner_self.sku_id = self.db.skus.insert_one(inner_self.sku).inserted_id
-            self.db.projects.update({"_id":inner_self.project_id},{"$push":{"skus":inner_self.sku_id}})
+            self.db.projects.update({"_id": inner_self.project_id}, {"$push": {"skus": inner_self.sku_id}})
+
         def teardown(inner_self):
-            self.db.projects.update({"_id":inner_self.project_id},{"$pull":{"skus":inner_self.sku_id}})
+            self.db.projects.update({"_id": inner_self.project_id}, {"$pull": {"skus": inner_self.sku_id}})
             self.db.skus.remove({"_id": inner_self.sku_id})
+
         return self.create_case("addRowsToSKU", setup, run, teardown)
-    
+
     def fetchAllUserComments(self):
         def setup(inner_self):
             pass
+
         # inner_self.user_id = self.get_random_id("users")
         def run(inner_self):
             raise NotImplementedError
+
         # inner_self.project_id = self.get_random_id("projects")
         #     inner_self.project_images = self.db.projects.find({"_id":inner_self.project_id},{"images":1, "_id":0})
         #     for image in inner_self.project_images: 
         def teardown(inner_self):
             pass
-            
+
         return self.create_case("fetchAllUserComments", setup, run, teardown)
-        
+
     def get_random_id(self, entity_name):
         from random import randint
         container = []
@@ -332,41 +339,41 @@ class Mongo(Base):
         random = randint(0, len(container) - 1)
         return container[random]
 
-	# Run project on: python main.py
-    
-   
-   
-#    test_cases = {
-#     'skim': [
-#         #'fetchSKU',
-#         #'fetchUsers',
-#         #'commentOnImage',
-#         #'pairImageSKU',
-#         #'addRowsToSKU',
-#         #'fetchAllUserComments'
-#         #'easy_get',
-#         #'easy_get2'
-#     ],
-#     'raceone': [
-#         #'follow',
-#         #'unfollow',
-#         'insertCoords',
-#         #'fetchParticipants',
-#         #'fetchParticipants2',
-#         #'unparticipate',
-#         #'fetchCoords',
-#         #'removeCoords',
-#         #'fetchHotRaces',
-#         #'fetchRace'
-#     ]
-# }
+    # Run project on: python main.py
 
-   
+
+
+    #    test_cases = {
+    #     'skim': [
+    #         #'fetchSKU',
+    #         #'fetchUsers',
+    #         #'commentOnImage',
+    #         #'pairImageSKU',
+    #         #'addRowsToSKU',
+    #         #'fetchAllUserComments'
+    #         #'easy_get',
+    #         #'easy_get2'
+    #     ],
+    #     'raceone': [
+    #         #'follow',
+    #         #'unfollow',
+    #         'insertCoords',
+    #         #'fetchParticipants',
+    #         #'fetchParticipants2',
+    #         #'unparticipate',
+    #         #'fetchCoords',
+    #         #'removeCoords',
+    #         #'fetchHotRaces',
+    #         #'fetchRace'
+    #     ]
+    # }
+
+
     # RACEONE
     def follow(self):
         def setup(inner_self):
             race_id = self.get_random_id("races")
-            race = self.db["races"].find_one({"_id":race_id},{"coordinates":0})
+            race = self.db["races"].find_one({"_id": race_id}, {"coordinates": 0})
             random = randint(0, len(race["activities"]) - 1)
             participant_id = race["activities"][random]["participating"]
             follower_id = self.get_random_id("users")
@@ -377,17 +384,21 @@ class Mongo(Base):
             inner_self.follower_id = follower_id
 
         def run(inner_self):
-            self.db["races"].update({"_id":inner_self.race_id,"activities":{"$elemMatch":{"participating":inner_self.participant_id}}},{"$push":{"activities.$.following":inner_self.follower_id}})
-        
+            self.db["races"].update(
+                {"_id": inner_self.race_id, "activities": {"$elemMatch": {"participating": inner_self.participant_id}}},
+                {"$push": {"activities.$.following": inner_self.follower_id}})
+
         def teardown(inner_self):
-            self.db["races"].update({"_id":inner_self.race_id,"activities":{"$elemMatch":{"participating":inner_self.participant_id}}},{"$pull":{"activities.$.following":inner_self.follower_id}})
+            self.db["races"].update(
+                {"_id": inner_self.race_id, "activities": {"$elemMatch": {"participating": inner_self.participant_id}}},
+                {"$pull": {"activities.$.following": inner_self.follower_id}})
 
         return self.create_case("follow", setup, run, teardown)
 
     def unfollow(self):
         def setup(inner_self):
             race_id = self.get_random_id("races")
-            race = self.db["races"].find_one({"_id":race_id},{"coordinates":0})
+            race = self.db["races"].find_one({"_id": race_id}, {"coordinates": 0})
             random = randint(0, len(race["activities"]) - 1)
             participant_id = race["activities"][random]["participating"]
             random2 = randint(0, len(race["activities"][random]["following"]) - 1)
@@ -397,19 +408,21 @@ class Mongo(Base):
             inner_self.follower_id = follower_id
 
         def run(inner_self):
-            self.db["races"].update({"_id":inner_self.race_id,"activities":{"$elemMatch":{"participating":inner_self.participant_id}}},{"$pull":{"activities.$.following":inner_self.follower_id}})
-        
+            self.db["races"].update(
+                {"_id": inner_self.race_id, "activities": {"$elemMatch": {"participating": inner_self.participant_id}}},
+                {"$pull": {"activities.$.following": inner_self.follower_id}})
+
         def teardown(inner_self):
-            self.db["races"].update({"_id":inner_self.race_id,"activities":{"$elemMatch":{"participating":inner_self.participant_id}}},{"$push":{"activities.$.following":inner_self.follower_id}})
+            self.db["races"].update(
+                {"_id": inner_self.race_id, "activities": {"$elemMatch": {"participating": inner_self.participant_id}}},
+                {"$push": {"activities.$.following": inner_self.follower_id}})
 
         return self.create_case("unfollow", setup, run, teardown)
-
-
 
     def insertCoords(self):
         def setup(inner_self):
             race_id = self.get_random_id("races")
-            race = self.db["races"].find_one({"_id":race_id},{"coordinates":0})
+            race = self.db["races"].find_one({"_id": race_id}, {"coordinates": 0})
             random = randint(0, len(race["activities"]) - 1)
             participant_id = race["activities"][random]["participating"]
             inner_self.race_id = race_id
@@ -417,19 +430,23 @@ class Mongo(Base):
             inner_self.coords = []
             for i in range(100):
                 inner_self.coords.append({
-                        "lat": 10 + i,
-                        "lng": 11 + i,
-                        "alt": 20 + i,
-                        "createdAt":datetime.datetime.now()
-                    })
+                    "lat": 10 + i,
+                    "lng": 11 + i,
+                    "alt": 20 + i,
+                    "createdAt": datetime.datetime.now()
+                })
 
         def run(inner_self):
             for coord in inner_self.coords:
-                self.db["races"].update({"_id":inner_self.race_id,"activities":{"$elemMatch":{"participating":inner_self.participant_id}}},{"$push":{"activities.$.coordinates":coord}})
+                self.db["races"].update({"_id": inner_self.race_id,
+                                         "activities": {"$elemMatch": {"participating": inner_self.participant_id}}},
+                                        {"$push": {"activities.$.coordinates": coord}})
 
         def teardown(inner_self):
             for coord in inner_self.coords:
-                self.db["races"].update({"_id":inner_self.race_id,"activities":{"$elemMatch":{"participating":inner_self.participant_id}}},{"$pull":{"activities.$.coordinates":coord}})
+                self.db["races"].update({"_id": inner_self.race_id,
+                                         "activities": {"$elemMatch": {"participating": inner_self.participant_id}}},
+                                        {"$pull": {"activities.$.coordinates": coord}})
 
         return self.create_case("insertCoords", setup, run, teardown)
 
@@ -440,7 +457,8 @@ class Mongo(Base):
         def run(inner_self):
             self.db["races"].find()
             cursor = self.cnx.cursor()
-            cursor.execute("SELECT participant.id, count(*) as followCount FROM participant INNER JOIN activity ON activity.participant=participant.id GROUP BY participant.id ORDER BY followCount")
+            cursor.execute(
+                "SELECT participant.id, count(*) as followCount FROM participant INNER JOIN activity ON activity.participant=participant.id GROUP BY participant.id ORDER BY followCount")
             result = cursor.fetchall()
             cursor.close()
 
@@ -485,6 +503,7 @@ class Mongo(Base):
     def fetchParticipants2(self):
         def setup(inner_self):
             pass
+
         def run(inner_self):
             pass
 
@@ -496,32 +515,36 @@ class Mongo(Base):
     def unparticipate(self):
         def setup(inner_self):
             race_id = self.get_random_id("races")
-            race = self.db["races"].find_one({"_id":race_id},{"coordinates":0})
+            race = self.db["races"].find_one({"_id": race_id}, {"coordinates": 0})
             random = randint(0, len(race["activities"]) - 1)
             participant_id = race["activities"][random]["participating"]
             inner_self.race_id = race_id
             inner_self.participant_id = participant_id
             inner_self.activity = race["activities"][random]
-        
+
         def run(inner_self):
-            self.db["races"].update({"_id":inner_self.race_id},{"$pull":{"activities":{"participating":inner_self.participant_id}}})
+            self.db["races"].update({"_id": inner_self.race_id},
+                                    {"$pull": {"activities": {"participating": inner_self.participant_id}}})
 
         def teardown(inner_self):
-            self.db["races"].update({"_id":inner_self.race_id},{"$push":{"activities":inner_self.activity}})
-            
+            self.db["races"].update({"_id": inner_self.race_id}, {"$push": {"activities": inner_self.activity}})
+
         return self.create_case("unparticipate", setup, run, teardown)
 
     def fetchCoords(self):
         def setup(inner_self):
             race_id = self.get_random_id("races")
-            race = self.db["races"].find_one({"_id":race_id},{"coordinates":0})
+            race = self.db["races"].find_one({"_id": race_id}, {"coordinates": 0})
             random = randint(0, len(race["activities"]) - 1)
             participant_id = race["activities"][random]["participating"]
             inner_self.race_id = race_id
             inner_self.participant_id = participant_id
 
         def run(inner_self):
-            race = self.db["races"].find_one({"_id":inner_self.race_id},{"coordinates":0,"activities":{"$elemMatch":{"participating":inner_self.participant_id}},"activities.participating":0,"activities.following":0,"activities.joinedAt":0})
+            race = self.db["races"].find_one({"_id": inner_self.race_id}, {"coordinates": 0, "activities": {
+                "$elemMatch": {"participating": inner_self.participant_id}}, "activities.participating": 0,
+                                                                           "activities.following": 0,
+                                                                           "activities.joinedAt": 0})
             coordinates = race["activities"][0]["coordinates"]
 
         def teardown(inner_self):
@@ -532,29 +555,31 @@ class Mongo(Base):
     def removeCoords(self):
         def setup(inner_self):
             race_id = self.get_random_id("races")
-            race = self.db["races"].find_one({"_id":race_id},{"activities":0})
+            race = self.db["races"].find_one({"_id": race_id}, {"activities": 0})
             coordinates = race["coordinates"]
             random.shuffle(coordinates)
             inner_self.race_id = race_id
-            inner_self.coordinates = coordinates[:(len(coordinates)//3)]
+            inner_self.coordinates = coordinates[:(len(coordinates) // 3)]
 
         def run(inner_self):
-            self.db["races"].update({"_id":inner_self.race_id},{"$pull":{"coordinates":{"$in":inner_self.coordinates}}})
+            self.db["races"].update({"_id": inner_self.race_id},
+                                    {"$pull": {"coordinates": {"$in": inner_self.coordinates}}})
 
         def teardown(inner_self):
-            self.db["races"].update({"_id":inner_self.race_id},{"$push":{"coordinates":{"$each":inner_self.coordinates}}})
+            self.db["races"].update({"_id": inner_self.race_id},
+                                    {"$push": {"coordinates": {"$each": inner_self.coordinates}}})
 
         return self.create_case("removeCoords", setup, run, teardown)
 
     def removeRace(self):
         def setup(inner_self):
             race_id = self.get_random_id("races")
-            race = self.db["races"].find_one({"_id":race_id})
+            race = self.db["races"].find_one({"_id": race_id})
             inner_self.race = race
-            inner_self.race_id = race_id 
-               
+            inner_self.race_id = race_id
+
         def run(inner_self):
-            self.db["races"].remove({"_id":inner_self.race_id})
+            self.db["races"].remove({"_id": inner_self.race_id})
 
         def teardown(inner_self):
             self.db.races.insert_one(inner_self.race)
@@ -566,8 +591,9 @@ class Mongo(Base):
             pass
 
         def run(inner_self):
-            race = self.db.races.aggregate([{"$unwind": '$activities'},{"$group": {"_id": '$_id',"totalsize": {"$sum": {"$size": '$activities.following'}}}},{"$limit":10}])
-            
+            race = self.db.races.aggregate([{"$unwind": '$activities'}, {
+                "$group": {"_id": '$_id', "totalsize": {"$sum": {"$size": '$activities.following'}}}}, {"$limit": 10}])
+
         def teardown(inner_self):
             pass
 
