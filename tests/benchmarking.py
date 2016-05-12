e20c0145058d0272484b4a1ef909747630ffb99f
Scanniza <simon@cymonit.se>
2016-05-12 17:05:48 +0200

    fixed removeRace mongo

--- tests/benchmarking.py
+++ tests/benchmarking.py
@@ -23,17 +23,17 @@ test_cases = {
         'fetchAllUserComments'
     ],
     'raceone': [
-        'follow',
-        'unfollow',
-        'insertCoords',
-        'fetchParticipants',
-        'fetchParticipants2',
-        'unparticipate',
-        'fetchCoords',
-        'removeCoords',
-        'fetchHotRaces',
-        'fetchRace',
-        'removeRace'
+#        'follow',
+ #       'unfollow',
+  #      'insertCoords',
+   #     'fetchParticipants',
+    #    'fetchParticipants2',
+     #   'unparticipate',
+      #  'fetchCoords',
+       # 'removeCoords',
+        #'fetchHotRaces',
+        #'fetchRace',
+        #'removeRace'
     ],
     'reference': [
         'tinyGet',
